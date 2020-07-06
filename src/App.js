fdbb0471a639ed4102cc5958f790c069f277bdfe
IndhuVelu <indhu31599@gmail.com>
2020-07-06 16:01:49 +0530

    calender_extension

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="full">
+      <div className="logo"><img src="https://images.squarespace-cdn.com/content/5b2bda42cc8fed2d1f0118d2/1529613311473-3XH6F88AOIA45ADX956W/Spark_Logo.png?content-type=image%2Fpng" alt="logo" width="90px" height="70px"/></div>
+       </div>
+       <div className="container_design">
+            <div> Meeting</div>
+            <div> Event Date : 24 Feb 2020</div>
+             <div> Event Starting Time :  10.30AM</div>
+             <div> Event Ending Time :  11.30Am</div>
+        </div>
+        <div >
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:3002"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        My Calender
         </a>
-      </header>
+         
+          </div>
     </div>
   );
 }
